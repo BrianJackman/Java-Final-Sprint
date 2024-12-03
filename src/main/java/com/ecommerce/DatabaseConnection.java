a75c799f2cc97ba3e2b72014baf02bee523ca096
Brian Jackman <brian.jackman@keyin.com>
2024-12-03 18:36:59 -0330

    fixed mistake with DB connection details.

--- src/main/java/com/ecommerce/DatabaseConnection.java
+++ src/main/java/com/ecommerce/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/commerce";
-    private static final String USER = "your_username";
-    private static final String PASSWORD = "your_password";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "PostgresPass";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
